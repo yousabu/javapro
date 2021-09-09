7fbac2085e04578da704ee7223841ba04847cd0a
youssef <shbanmhmdywsf8@gmail.com>
2021-09-09 11:01:18 +0200

    from local

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+
+    public static void main(String[] args){
+
+        for (int i=1;i<20;i++){
+            System.out.println("Hi Youssef From Java program ");
+        } 
+    }
+
+
+}
